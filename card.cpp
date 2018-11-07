f904442299235d9dc579983ae5bf6f7571309dfb
Vishruthi Ramaswamy <vishruthi@wireless-10-148-187-151.public.utexas.edu>
2018-11-06 18:18:21 -0600

    Added Functions to cpp file

--- card.cpp
+++ card.cpp
@@ -1,1 +1,34 @@
+#include<stdlib.h>
+#include<string>
+#include<stdbool.h>
 #include "card.h"
+
+using namespace std;
+
+string Card::toString() const {
+
+}
+
+bool Card::sameSuitAs(const Card &c) const {
+
+}
+
+int Card::getRank() const {
+
+}
+
+string Card::suitString(Card::Suit s) const {
+
+}
+
+string Card::rankString(int r) const {
+
+}
+
+bool Card::operator==(const Card &rhs) const {
+
+}
+
+bool Card::operator!=(const Card &rhs) const {
+
+}
