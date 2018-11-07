28952183b7e816e31dbd941133380c45a15e328a
Vishruthi Ramaswamy <vishruthi@wireless-10-148-187-151.public.utexas.edu>
2018-11-06 19:16:57 -0600

    Worked on player.cpp

--- player.cpp
+++ player.cpp
@@ -2,4 +2,38 @@
 // Created by Vishruthi Ramaswamy on 11/5/18.
 //
 
+#include <iostream>
+#include <string>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <vector>
 #include "player.h"
+
+
+void Player::addCard(Card c) {
+    myHand.push_back(c);
+}
+Card Player::removeCardFromHand(Card c) {
+    myHand.erase(find(myHand.begin(), myHand.end(), c));
+    return c;
+}
+
+void Player::bookCards(Card c1, Card c2) {}
+
+Card Player::chooseCardFromHand() const {}
+
+bool Player::cardInHand(Card c) const {}
+
+bool Player::checkHandForPair(Card &c1, Card &c2) {}
+
+int Player::getBookSize() const {}
+
+int Player::getHandSize() const {}
+
+
+
+string Player::showBooks() const {}
+
+string Player::showHand() const {}
+
+bool Player ::sameRankInHand(Card c) const {}
--- player.h
+++ player.h
@@ -34,13 +34,6 @@ public:
 
     //OPTIONAL
     // comment out if you decide to not use it
-    //this function will check a players hand for a pair.
-    //If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
-
-    bool checkHandForBook(Card &c1, Card &c2);
-
-    //OPTIONAL
-    // comment out if you decide to not use it
     //Does the player have a card with the same rank as c in her hand?
     bool rankInHand(Card c) const;
 
